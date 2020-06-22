eaed93735dded28b84e96a98e3756a83d0580f68
Victorhmartinez <victormartinezg1998@gmail.com>
2020-06-21 20:11:33 -0500

    Comenzando proyecto 2

---
+++ src/main/java/SAF/GUI/Princiapl.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package SAF.GUI;
+//import org.springframework.boot.SpringApplication;
+
+import javax.swing.JOptionPane;
+
+/**
+ *
+ * @author Usuario
+ */
+
+public class Princiapl {
+    public static void main(String[] args) {
+        JOptionPane.showMessageDialog(null, "Hola probando Jankis");
+        System.out.println("x2 esto es una prueba");
+    }
+}
