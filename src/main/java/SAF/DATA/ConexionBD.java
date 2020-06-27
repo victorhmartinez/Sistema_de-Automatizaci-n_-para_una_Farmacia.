445fab24befcc8ad5034eedcafc20c769e671474
Usuario <jonahg10.11@gmail.com>
2020-06-27 16:58:31 -0500

    Creando conexio general a la BD

--- src/main/java/SAF/DATA/ConexionBD.java
+++ src/main/java/SAF/DATA/ConexionBD.java
@@ -5,10 +5,45 @@
  */
 package SAF.DATA;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import javax.swing.JOptionPane;
+
 /**
  *
  * @author Usuario
  */
 public class ConexionBD {
     
+    
+     //Conectarse a la BDD
+   public Connection con;
+   public String driver = "com.mysql.jdbc.Driver";
+    public String user = "root";
+    public String password = "";
+    public String url = "jdbc:mysql://localhost:3306/bfarmacia";
+    
+   //mismo nombre de la clase
+    public ConexionBD() {
+        
+        try {
+            Class.forName(driver);
+            con = DriverManager.getConnection(url, user, password);
+           if (con !=null) {
+               JOptionPane.showMessageDialog(null, "Conexion Exitosa");
+            }
+        } catch (Exception e) {
+            JOptionPane.showMessageDialog(null, "Error al intentar conectarse con la BD");
+        }
+    }
+    public Connection conectarBD(){
+        return con;
+    }
+    public void desconectarBD(){
+        con = null;
+        if (con==null) {
+          JOptionPane.showMessageDialog(null, "Conexion cerrada...");
+
+        }
+    }
 }
